950988f17dc653bc7ef757a82513c1c31c74d1de
Patrick Boynton <youremail@yourdomain.com>
2024-02-15 10:28:06 -0500

    End of section 2

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,34 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+	DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+	Console.WriteLine("An error occured during initialization." + e);
+	throw;
+}
+
+app.Run();
