39e03a197a77147f9520772a8d6b161261b37a6d
Patrick Boynton <youremail@yourdomain.com>
2024-06-05 15:03:00 -0400

    Updated and formatted the search controller.

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,48 +9,50 @@ namespace SearchService.Controllers;
 [Route("api/[controller]")]
 public class SearchController : ControllerBase
 {
-	[HttpGet]
-	public async Task<ActionResult<Item>> SearchItems([FromQuery] SearchParams searchParams)
-	{
-		var query = DB.PagedSearch<Item, Item>();
+    [HttpGet]
+    public async Task<ActionResult<Item>> SearchItems([FromQuery] SearchParams searchParams)
+    {
+        var query = DB.PagedSearch<Item, Item>();
 
-		query.Sort(x => x.Ascending(y => y.Make));
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+        {
+            Console.WriteLine($"Searching for {searchParams.SearchTerm}");
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
 
-		if (!string.IsNullOrWhiteSpace(searchParams.SearchTerm))
-			query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        Console.WriteLine($"Ordering by {searchParams.OrderBy}");
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(y => y.Make)),
+            "model" => query.Sort(x => x.Ascending(y => y.Model)),
+            "new" => query.Sort(x => x.Descending(y => y.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(y => y.AuctionEnd))
+        };
 
-		query = searchParams.OrderBy switch
-			{
-				"make" => query.Sort(x => x.Ascending(y => y.Make)),
-				"model" => query.Sort(x => x.Ascending(y => y.Model)),
-				"new" => query.Sort(x => x.Ascending(y => y.CreatedAt)),
-				_ => query.Sort(x => x.Ascending(y => y.AuctionEnd))
-			};
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x =>
+                x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
 
-		query = searchParams.FilterBy switch
-			{
-				"finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-				"endingSoon" => query.Match(x =>
-					x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
-				_ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
-			};
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+            query.Match(x => x.Seller == searchParams.Seller);
 
-		if (!string.IsNullOrWhiteSpace(searchParams.Seller))
-			query.Match(x => x.Seller == searchParams.Seller);
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+            query.Match(x => x.Winner == searchParams.Winner);
 
-		if (!string.IsNullOrWhiteSpace(searchParams.Winner))
-			query.Match(x => x.Winner == searchParams.Winner);
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
-		query.PageNumber(searchParams.PageNumber);
-		query.PageSize(searchParams.PageSize);
+        var result = await query.ExecuteAsync();
 
-		var result = await query.ExecuteAsync();
-
-		return Ok(new
-			{
-				results = result.Results,
-				pageCount = result.PageCount,
-				totalCount = result.TotalCount
-			});
-	}
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
+    }
 }
