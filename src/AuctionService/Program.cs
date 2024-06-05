c5e2f210efbda5b7c8abc735523e4be9c0bb7f73
Patrick Boynton <youremail@yourdomain.com>
2024-06-05 15:02:35 -0400

    Fixed an issue with the rmq setup.

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -13,41 +13,42 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<AuctionDbContext>(options =>
 {
-	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddMassTransit(x =>
 {
-	x.AddEntityFrameworkOutbox<AuctionDbContext>(options =>
-	{
-		options.QueryDelay = TimeSpan.FromMinutes(6);
-		options.UsePostgres();
-		options.UseBusOutbox();
-	});
-	x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(options =>
+    {
+        options.QueryDelay = TimeSpan.FromSeconds(100);
+        options.UsePostgres();
+        options.UseBusOutbox();
+    });
 
-	x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
 
-	x.UsingRabbitMq((context, cfg) =>
-	{
-		cfg.Host(builder.Configuration["RabbitMq:Host"], "/",
-			host =>
-			{
-				host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-				host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
-			});
-		cfg.ConfigureEndpoints(context);
-	});
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/",
+            host =>
+            {
+                host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+                host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+            });
+        cfg.ConfigureEndpoints(context);
+    });
 });
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
-	options.Authority = builder.Configuration["IdentityServiceUrl"];
-	options.RequireHttpsMetadata = false;
-	options.TokenValidationParameters.ValidateAudience = false;
-	options.TokenValidationParameters.NameClaimType = "username";
+    options.Authority = builder.Configuration["IdentityServiceUrl"];
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters.ValidateAudience = false;
+    options.TokenValidationParameters.NameClaimType = "username";
 });
 
 
@@ -61,12 +62,12 @@ app.MapControllers();
 
 try
 {
-	DbInitializer.InitDb(app);
+    DbInitializer.InitDb(app);
 }
 catch (Exception e)
 {
-	Console.WriteLine("An error occured during initialization." + e);
-	throw;
+    Console.WriteLine("An error occured during initialization." + e);
+    throw;
 }
 
 app.Run();
