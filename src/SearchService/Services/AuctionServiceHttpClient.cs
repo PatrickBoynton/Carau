961b5919a42d433d50b2149c42f4332cc54e8212
Patrick Boynton <youremail@yourdomain.com>
2024-06-05 15:00:45 -0400

    Updated the search db function.

--- src/SearchService/Services/AuctionServiceHttpClient.cs
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using MongoDB.Entities;
 using SearchService.Models;
 
@@ -5,14 +6,17 @@ namespace SearchService.Services;
 
 public class AuctionServiceHttpClient(HttpClient client, IConfiguration configuration)
 {
-	public async Task<List<Item>?> GetItemsForSearchDb()
-	{
-		var lastUpdated = await DB.Find<Item, string>()
-			.Sort(x => x.Descending(y => y.UpdatedAt))
-			.Project(x => x.UpdatedAt.ToString())
-			.ExecuteFirstAsync();
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, DateTime>()
+            .Sort(x => x.Descending(y => y.UpdatedAt))
+            .Project(x => x.UpdatedAt)
+            .ExecuteFirstAsync();
 
-		return await client.GetFromJsonAsync<List<Item>>(configuration["AuctionServiceUrl"] +
-		                                                 "/api/auctions?date=" + lastUpdated);
-	}
+        var lastUpdatedString = lastUpdated.ToString(CultureInfo.InvariantCulture);
+
+        return await client.GetFromJsonAsync<List<Item>>(
+                   configuration["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdatedString) ??
+               throw new InvalidOperationException();
+    }
 }
